9f4245cb4da39fb019a1ee937a3570d36ae1c66f
ProducerZekServer <go@away.com>
2021-06-21 01:09:47 +0000

    Added a better buff fade check to sky

--- airplane/player.pl
+++ airplane/player.pl
@@ -1,6 +1,12 @@
 sub EVENT_ENTERZONE {
-	if ($status < 80) {
-		$client->BuffFadeAll();
-	}
+    if ($status < 80) {
+        if($client->BuffCount()) {
+            for($i=0; $i < $client->BuffCount(); $i++) {
+                if($client->FindBuffBySlot($i) !~ [756, 757, 5249])    {#:: Rezz sickness
+                    $client->BuffFadeBySlot($i);
+                }
+            }    
+        }
+    }
 	$client->Message(13, "You have entered a zone with no PvP level range restrictions."); 
 }
