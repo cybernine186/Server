8f452bd658e8527fd03701eebc7aa7478c3c345a
ProducerZekServer <go@away.com>
2021-06-21 01:08:48 +0000

    Added a team array to YTs

--- global/global_player.pl
+++ global/global_player.pl
@@ -1,6 +1,23 @@
 
 sub EVENT_DEATH_COMPLETE  {
 	$guildslain = quest::getguildnamebyid($uguild_id);
+	my %team = (
+		201 => Evil,
+		203 => Evil,
+		206 => Evil,
+		202 => Neutral,
+		205 => Neutral,
+		207 => Neutral,
+		209 => Neutral,
+		213 => Neutral,
+		214 => Neutral,
+		216 => Neutral,
+		204 => Good,
+		208 => Good,
+		210 => Good,
+		212 => Good,
+		215 => Good
+		);
 	
 	if ($killer_id == 0 && length($killer_name) > 0) {
 		$connect = plugin::LoadMysql();
@@ -27,28 +44,30 @@ sub EVENT_DEATH_COMPLETE  {
 		
 	}
 	
-	if ($entity_list->GetMobByID($killer_id)->IsClient()) {
+	if ($entity_list->GetMobByID($killer_id) && $entity_list->GetMobByID($killer_id)->IsClient()) {
 		$characteridbymob = $entity_list->GetClientByID($killer_id)->CharacterID();
 		$guildid = quest::getguildidbycharid($characteridbymob);
 		$guildname = quest::getguildnamebyid($guildid);
+		$killerdeitycheck = $entity_list->GetMobByID($killer_id)->GetDeity();
+		$killeddeitycheck = $client->GetDeity();
 		
-		#quest::gmsay("[PVP] $name of <$guildslain> has been killed by " . ($entity_list->GetMobByID($killer_id) ? $entity_list->GetMobByID($killer_id)->GetCleanName() : "an unknown hand") . " of <$guildname> in $zoneln!", 315, 1, 0, 0);
+		quest::gmsay("[PVP] $name <$guildslain> of the $team{$killeddeitycheck} team has been killed by " . ($entity_list->GetMobByID($killer_id) ? $entity_list->GetMobByID($killer_id)->GetCleanName() : "an unknown hand") . " <$guildname> of the $team{$killerdeitycheck} team in $zoneln!", 315, 1, 0, 0);
 
-		quest::we(315,"[PVP] $name of <$guildslain> has been killed by  " . ($entity_list->GetMobByID($killer_id) ? $entity_list->GetMobByID($killer_id)->GetCleanName() : "an unknown hand") . " of <$guildname> in $zoneln!"); 
-		#Lets use world emote instead of GM say to see if it picks up on Discord EQ bot
 		return;
 		
 		
 	}
 	
-	if ($entity_list->GetMobByID($killer_id)->IsPet()) {
+	if ($entity_list->GetMobByID($killer_id) && $entity_list->GetMobByID($killer_id)->IsPet()) {
 		$petowner = $entity_list->GetMobByID($killer_id)->GetOwnerID();
 		$characteridbymob = $entity_list->GetClientByID($petowner)->CharacterID();
 		$guildid = quest::getguildidbycharid($characteridbymob);
 		$guildname = quest::getguildnamebyid($guildid);
+		$killerdeitycheck = $entity_list->GetMobByID($petowner)->GetDeity();
+		$killeddeitycheck = $client->GetDeity();
 
-		#uest::gmsay("[PVP] $name of <$guildslain> has been killed by " . ($entity_list->GetMobByID($petowner) ? $entity_list->GetMobByID($petowner)->GetCleanName() : "an unknown hand") . " of <$guildname> in $zoneln!", 315, 1, 0, 0);
-		quest::we(315,"[PVP] $name of <$guildslain> has been killed by  " . ($entity_list->GetMobByID($petowner) ? $entity_list->GetMobByID($petowner)->GetCleanName() : "an unknown hand") . " of <$guildname> in $zoneln!"); 
+
+		quest::gmsay("[PVP] $name <$guildslain> of the $team{$killeddeitycheck} team has been killed by " . ($entity_list->GetMobByID($petowner) ? $entity_list->GetMobByID($petowner)->GetCleanName() : "an unknown hand") . " <$guildname> of the $team{$killerdeitycheck} team in $zoneln!", 315, 1, 0, 0);
 
 		
 		return; 
@@ -116,10 +135,6 @@ sub EVENT_LEVEL_UP {
 	$Racecheck = $client->GetRace();
 	$cleanrace = quest::getracename($Racecheck);
 	
-	if($levelcheck == 5 or $levelcheck == 10 or $levelcheck == 15 or $levelcheck == 20 or $levelcheck == 25 or $levelcheck == 30 or $levelcheck == 35 or $levelcheck == 40 or $levelcheck == 45 or $levelcheck == 50 or $levelcheck == 55 or $levelcheck == 60) {
-		quest::gmsay("[Achievement] Congratulations to $namecheck of <$guildname> for becoming the first level $levelcheck $cleanrace $cleanclass!", 257, 1, 0, 0); 
-	}
-	
 	if($classcheck == 1 or $classcheck == 7 or $classcheck == 9) {
 		if (quest::is_the_ruins_of_kunark_enabled()) {
 			quest::traindiscs($levelcheck,($levelcheck - 1));
@@ -129,6 +144,21 @@ sub EVENT_LEVEL_UP {
 		if (quest::is_the_scars_of_velious_enabled()) {
 			quest::traindiscs($levelcheck,($levelcheck - 1));
 		}
+	}
+}
+
+sub EVENT_SERVERFIRST_LEVEL {
+	$levelcheck = $client->GetLevel();
+	$classcheck = $client->GetClass();
+	$namecheck = $client->GetCleanName();
+	$guildnamecheck = $client->GuildID();
+	$guildname = quest::getguildnamebyid($guildnamecheck);
+	$cleanclass = quest::getclassname($classcheck);
+	$Racecheck = $client->GetRace();
+	$cleanrace = quest::getracename($Racecheck);
+	
+	if($levelcheck == 5 or $levelcheck == 10 or $levelcheck == 15 or $levelcheck == 20 or $levelcheck == 25 or $levelcheck == 30 or $levelcheck == 35 or $levelcheck == 40 or $levelcheck == 45 or $levelcheck == 50 or $levelcheck == 55 or $levelcheck == 60) {
+		quest::gmsay("[Achievement] Congratulations to $namecheck of <$guildname> for becoming the first level $levelcheck $cleanrace $cleanclass!", 257, 1, 0, 0); 
 	}
 }
 
