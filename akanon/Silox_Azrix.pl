5671fd52d8ca5a21765b4604e6731bb7f4dd5393
brynja-eq <88106956+brynja-eq@users.noreply.github.com>
2021-09-22 12:56:32 -0700

    conditional blocks added in front of epic quest dialog and hand ins. kunark test.

--- akanon/Silox_Azrix.pl
+++ akanon/Silox_Azrix.pl
@@ -1,15 +1,17 @@
-sub EVENT_SAY {
-	if($text=~/hail/i) {
-		quest::say("gives you a crooked smile. 'Hello there, $name. I'm doin some important 'meditations' to my god here. Maybe you can go somewhere else?'");
+#Druid Epic
+if(quest::is_the_ruins_of_kunark_enabled()){
+	sub EVENT_SAY {
+		if($text=~/hail/i) {
+			quest::say("gives you a crooked smile. 'Hello there, $name. I'm doin some important 'meditations' to my god here. Maybe you can go somewhere else?'");
+		}
 	}
-}
-sub EVENT_ITEM {
-	if ($itemcount{20694} == 1) {
-		quest::emote(" eye light up. Finally, I can now be cleansed of the dark god within. Thank you $name. Here, take this as a token of my heart felt Thanks");
-		quest::summonitem(20697);
-		quest::depop_withtimer();
-	} else {
-		quest::emote("will not take this item.");
-		plugin::return_items(\%itemcount);
+	sub EVENT_ITEM {
+		if ($itemcount{20694} == 1) {
+			quest::emote("Silox Azrix looks up at you with a twisted gleam in his eyes. 'Something for me then, eh?' As he looks closer his skin begins to peel and stretch, a bright hot light emanating from his hands gripped around the unicorn horn. 'NOOOO!' The pure cleansing light burns away the gnome without a trace of his existence except for a tiny ball of light which you pick up.");
+			quest::summonitem(20697);
+			quest::depop_withtimer();
+		} else {
+			quest::emote("will not take this item.");
+			plugin::return_items(\%itemcount);
+		}
 	}
-}
