4abe8b9a9b6455a9fbfdfb6d79956bcc0203732a
cybernine <cybernine186@gmail.com>
2021-10-21 09:53:39 -0400

    Changed end of line format to match Linux systems so the scripts will run properly.

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "#########################################################                         "
-echo "#::: EverQuest Emulator Modular Installer                                         "
-echo "#::: Installer Author: Akkadius                                                   "
-echo "#::: Installer Co-Author(s): N0ctrnl                                              "
-echo "#:::                                                                              "
-echo "#::: EQEmulator Server Software is developed and maintained                       "
-echo "#:::	by the EQEmulator Developement team                                         "
-echo "#:::                                                                              "
-echo "#::: Everquest is a registered trademark                                          "
-echo "#::: Daybreak Game Company LLC.                                                   "
-echo "#:::                                                                              "
-echo "#::: EQEmulator is not associated or                                              "
-echo "#::: affiliated in any way with Daybreak Game Company LLC.                        "
+echo "# EverQuest Emulator Modular Installer                                         "
+echo "# Installer Author: Akkadius                                                   "
+echo "# Installer Co-Author(s): N0ctrnl                                              "
+echo "#                                                                              "
+echo "# EQEmulator Server Software is developed and maintained                       "
+echo "#	by the EQEmulator Developement team                                         "
+echo "#                                                                              "
+echo "# Everquest is a registered trademark                                          "
+echo "# Daybreak Game Company LLC.                                                   "
+echo "#                                                                              "
+echo "# EQEmulator is not associated or                                              "
+echo "# affiliated in any way with Daybreak Game Company LLC.                        "
 echo "#########################################################                         "
 echo "#:                                                                                "
 echo "#########################################################                         "
-echo "#::: To be installed:                                                             "
+echo "# To be installed:                                                             "
 echo "#########################################################                         "
 echo "- Server running folder - Will be installed to the folder you ran this script     "
 echo "- MariaDB (MySQL) - Database engine                                               "
@@ -46,7 +46,7 @@ if [ ! -f ./install_variables.txt ]; then
 
 	read -n1 -r -p "Press any key to continue..." key
 	
-	#::: Setting up user environment (eqemu)
+	# Setting up user environment (eqemu)
 	echo "First, we need to set your passwords..."
 	echo "Make sure that you remember these and keep them somewhere"
 	echo ""
@@ -55,22 +55,22 @@ if [ ! -f ./install_variables.txt ]; then
 	useradd -g eqemu -d $eqemu_server_directory eqemu
 	passwd eqemu
 
-	#::: Make server directory and go to it
+	# Make server directory and go to it
 	mkdir $eqemu_server_directory
 	cd $eqemu_server_directory
 
-	#::: Setup MySQL root user PW
+	# Setup MySQL root user PW
 	read -p "Enter MySQL root (Database) password: " eqemu_db_root_password
 
-	#::: Write install variables (later use)
+	# Write install variables (later use)
 	echo "mysql_root:$eqemu_db_root_password" > install_variables.txt
 
-	#::: Setup MySQL server 
+	# Setup MySQL server 
 	read -p "Enter Database Name (single word, no special characters, lower case):" eqemu_db_name
 	read -p "Enter (Database) MySQL EQEmu Server username: " eqemu_db_username
 	read -p "Enter (Database) MySQL EQEmu Server password: " eqemu_db_password
 
-	#::: Write install variables (later use)
+	# Write install variables (later use)
 	echo "mysql_eqemu_db_name:$eqemu_db_name" >> install_variables.txt
 	echo "mysql_eqemu_user:$eqemu_db_username" >> install_variables.txt
 	echo "mysql_eqemu_password:$eqemu_db_password" >> install_variables.txt
@@ -87,6 +87,7 @@ apt-get $apt_options install curl
 apt-get $apt_options install debconf-utils
 apt-get $apt_options install g++
 apt-get $apt_options install gcc
+apt-get $apt_options install gdb
 apt-get $apt_options install git
 apt-get $apt_options install git-core
 apt-get $apt_options install libio-stringy-perl
@@ -116,13 +117,6 @@ apt-get $apt_options install apache2
 apt-get $apt_options install php
 apt-get $apt_options install php-mbstring
 apt-get $apt_options install php-gettext
-apt-get $apt_options install phpmyadmin
-
-# PHPMyAdmin Install
-phpenmod mbstring
-ln -s -f /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
-a2enconf phpmyadmin
-systemctl restart apache2
 
 # Install libsodium
 wget http://ftp.us.debian.org/debian/pool/main/libs/libsodium/libsodium-dev_1.0.11-2_amd64.deb -O /home/eqemu/libsodium-dev.deb
@@ -133,7 +127,7 @@ dpkg -i /home/eqemu/libsodium*.deb
 rm -f /home/eqemu/libsodium-dev.deb
 rm -f /home/eqemu/libsodium18.deb
 
-#::: Install MariaDB Server
+# Install MariaDB Server
 export DEBIAN_FRONTEND=noninteractive
 debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password PASS'
 debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password PASS'
@@ -144,19 +138,27 @@ mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('$eqemu_db_root_password');"
 echo "Starting MariaDB server..."
 systemctl enable mariadb.service --now
 sleep 5
-/usr/bin/mysqladmin -u root password $eqemu_db_root_password
+mysql_secure_installation
 
-#::: Configure game server database user
+# Configure game server database user
+/usr/bin/mysqladmin -u root password $eqemu_db_root_password
 mysql -uroot -p$eqemu_db_root_password -e "CREATE USER '$eqemu_db_username'@'localhost' IDENTIFIED BY '$eqemu_db_password';"
 mysql -uroot -p$eqemu_db_root_password -e "GRANT GRANT OPTION ON *.* TO '$eqemu_db_username'@'localhost';"
 mysql -uroot -p$eqemu_db_root_password -e "GRANT ALL ON *.* TO '$eqemu_db_username'@'localhost';"
 
-#::: Create source and server directories
+# PHPMyAdmin Install
+apt-get $apt_options install phpmyadmin
+phpenmod mbstring
+ln -s -f /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
+a2enconf phpmyadmin
+systemctl restart apache2
+
+# Create source and server directories
 mkdir $eqemu_server_directory/server
 mkdir $eqemu_server_directory/server_build
 mkdir $eqemu_server_directory/server_source
 
-#::: Back to server directory
+# Back to server directory
 git clone --recurse-submodules https://github.com/cybernine186/Server.git $eqemu_server_directory/server
 git clone --recurse-submodules https://github.com/cybernine186/Code.git $eqemu_server_directory/server_source
 git clone --recurse-submodules https://github.com/cybernine186/EQEmuMaps.git $eqemu_server_directory/server/maps
@@ -165,29 +167,29 @@ git clone --recurse-submodules https://github.com/cybernine186/peqphpeditor.git 
 git clone --recurse-submodules https://github.com/cybernine186/Client.git $eqemu_server_directory/client
 git clone --recurse-submodules https://github.com/cybernine186/EQEmuAllakhazamClone.git $eqemu_server_directory/eqemuallakhazamclone
 
-#::: PEQEditor
+# PEQEditor
 ln -s -f $eqemu_server_directory/peqeditor/apache2/peqeditor.conf /etc/apache2/conf-available/peqeditor.conf
 a2enconf peqeditor
 systemctl restart apache2
 
-#::: EQEmuAllakhazamClone
+# EQEmuAllakhazamClone
 ln -s -f /home/eqemu/eqemuallakhazamclone/apache2/eqemuallakhazamclone.conf /etc/apache2/conf-available/eqemuallakhazamclone.conf
 a2enconf eqemuallakhazamclone
 systemctl restart apache2
 
-#::: Client
+# Client
 ln -s -f $eqemu_server_directory/client/apache2/eqemupatcher_00.conf /etc/apache2/conf-available/eqemupatcher_00.conf
 ln -s -f $eqemu_server_directory/client/apache2/eqemupatcher_01.conf /etc/apache2/conf-available/eqemupatcher_01.conf
 ln -s -f $eqemu_server_directory/client/apache2/eqemupatcher_02.conf /etc/apache2/conf-available/eqemupatcher_02.conf
 a2enconf eqemupatcher_00.conf
 systemctl restart apache2
 
-#::: Build Server
+# Build Server
 cd $eqemu_server_directory/server_build
 cmake $cmake_options -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_LUA=ON -G "Unix Makefiles" $eqemu_server_directory/server_source
 make -j 12
 
-#::: Link Server Executables
+# Link Server Executables
 ln -s -f $eqemu_server_directory/server/maps $eqemu_server_directory/server/Maps
 ln -s -f $eqemu_server_directory/server_build/bin/eqlaunch $eqemu_server_directory/server/eqlaunch
 ln -s -f $eqemu_server_directory/server_build/bin/export_client_files $eqemu_server_directory/server/export_client_files
@@ -201,7 +203,7 @@ ln -s -f $eqemu_server_directory/server_build/bin/world $eqemu_server_directory/
 ln -s -f $eqemu_server_directory/server_build/bin/zone $eqemu_server_directory/server/zone
 ln -s -f $eqemu_server_directory/server_build/bin/loginserver $eqemu_server_directory/server/loginserver
 
-#::: Chown files
+# Chown files
 chown eqemu:eqemu $eqemu_server_directory/ -R 
 chmod 755 $eqemu_server_directory/server/*.pl
 chmod 755 $eqemu_server_directory/server/*.sh
