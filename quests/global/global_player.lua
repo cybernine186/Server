1e21480f72868a316b4c42700de1aaadd126dd20
cybernine <cybernine186@gmail.com>
2021-11-04 22:53:46 -0400

    Added support for if pets kill players the owner gets the credit for world emote.

--- quests/global/global_player.lua
+++ quests/global/global_player.lua
@@ -1,23 +1,34 @@
 function event_death(e)
 
+	if(e.self:IsNPC()) then
+		return;
+	end
+
 	local victimName = eq.get_char_name_by_id(e.self:CharacterID());
 	local victimClass = e.self:GetClassName();
 	local victimGuildID = e.self:GuildID();
 	local victimGuild = eq.get_guild_name_by_id(victimGuildID);
-	local killerName = eq.get_char_name_by_id(e.other:CastToClient():CharacterID());
-	local killerClass = e.other:CastToClient():GetClassName();
-	local killerGuildID = e.other:CastToClient():GuildID();
+	local killerName;
+	local killerClass;
+	local killerGuildID;
+	
+	if(e.other:IsPet()) then
+		killerName = eq.get_char_name_by_id(e.other:GetOwner():CastToClient():CharacterID());
+		killerClass = e.other:GetOwner():CastToClient():GetClassName();
+		killerGuildID = e.other:GetOwner():CastToClient():GuildID();
+	else
+		killerName = eq.get_char_name_by_id(e.other:CastToClient():CharacterID());
+		killerClass = e.other:CastToClient():GetClassName();
+		killerGuildID = e.other:CastToClient():GuildID();
+	end
+	
 	local killerGuild = eq.get_guild_name_by_id(killerGuildID);
-	
-	--local zone = eq.get_zone_long_name_by_id(e.self:CastToClient():GetZoneID());
-	--local zone = e.other:CastToClient():GetZoneID();
-	
+	local zone = eq.get_zone_short_name_by_id(e.zoneid);
 	local weakArray = {'weakling', 'cowardly', 'lowly peasant', 'eunuch'}
-	local killArray = {'killed', 'slain', 'murdered', 'slaughtered', 'beaten to death', 'castrated', 'mutulated', 'mauled'}
-	local strongArray = {'great', 'strong', 'cutthroat', 'serial killer', 'murderer', 'champion', 'death dealer', 'sadistic'}
-	
-	local message = string.format("A %s %s of <%s> has been %s by a %s %s of <%s> in %s",
-		weakArray[math.random(#weakArray)], victimClass, victimGuild, killArray[math.random(#killArray)], strongArray[math.random(#strongArray)], killerClass, killerGuild, 'The Arena');
-	eq.world_emote(315, message);
+	local killArray = {'killed', 'slain', 'murdered', 'slaughtered', 'beat to death', 'castrated', 'mutulated', 'mauled'}
+	local strongArray = {'cutthroat', 'killer', 'murdering', 'death dealing', 'sadistic'}
 
+	local message = string.format("[%s] A %s %s of <%s> has been %s by a %s %s of <%s>.",
+		zone, weakArray[math.random(#weakArray)], victimClass, victimGuild, killArray[math.random(#killArray)], strongArray[math.random(#strongArray)], killerClass, killerGuild);
+	eq.world_emote(315, message);
 end
